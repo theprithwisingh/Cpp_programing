18bb6819d185b739fd4dde0deb0df4b10e0fb570
Prithwi singh <128086732+theprithwisingh@users.noreply.github.com>
2023-10-03 00:15:02 +0530

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,84 @@
+/*
+#include <iostream>
+using namespace std;
+int main(){
+    int i = 0;
+    while(i<=11){
+        cout<<i<<endl;
+        i++;
+    }
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+int main(){
+    int i = 0;
+    while(i<=115){
+        cout<<i<<endl;
+        i++;
+    }
+}
+*/
+/*
+#include <iostream>
+using namespace std;
+int main(){
+    int j = 2;
+    while(j<=10){
+        cout<<j<<endl;
+        j++;
+    }
+}
+*/
+
+
+//Garbage Value
+/*
+#include <iostream>
+using namespace std;
+int main(){
+    int j;
+    cout<<j;
+}
+*/
+//output -> a random value
+
+
+/*
+#include <iostream>
+using namespace std;
+int main(){
+    int x = 3;
+    while(x==3){
+        x = x - 1;
+        cout<<x<<endl;
+    }
+}
+*/
+
+
+//write a while loop that prints numbers from any consecutive 
+
+
+/*
+#include <iostream>
+using namespace std;
+int main() {
+   int i;
+   cout<<"enter value of i :";
+   cin>>i;
+
+   int n;
+   cout<<"enter value of n :";
+   cin>>n;
+
+   while (i<=n) {
+   cout << i << endl;
+   i++;
+   }
+}
+*/
+
+
